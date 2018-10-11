cba20f4274150662248b7c0d4aadf64dce4ed3e6
caicai <1841978799@qq.com>
2018-10-11 17:12:29 +0800

    add

--- service/module/login/Users.js
+++ service/module/login/Users.js
@@ -1,8 +1,14 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+/**
+ * 关于_id默认值：
+ * 不要使用default: new mongoose.Types.ObjectId(),这种方式重复插入会报错，如果使用这种方式
+ * 则插入数据时要指定_id字段：_id:  new mongoose.Types.ObjectId()
+ * 建议使用方法模式返回（如下）
+ */
 var usersSchema = new Schema({
-  _id: Schema.Types.ObjectId,
+  _id: {type: Schema.Types.ObjectId, default: function () { return new mongoose.Types.ObjectId() }},
   name: String,
   age: Number,
   sex: Number,
