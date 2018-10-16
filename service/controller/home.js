99bf37bb2b841cf7a2c600a3a15362bee7cc6d6f
RM_LOG <yezi_log@163.com>
2018-10-16 22:41:05 +0800

    add

--- service/controller/home.js
+++ service/controller/home.js
@@ -1,6 +1,13 @@
-let homeRouter = async (ctx, next) => {
-  let GoodsModule = require('../module/goods/Goods');
+let GoodsModule = require('../module/goods/Goods');
 
+let homeRouter = async (ctx, next) => {
+  ctx.body = 'home page';
+}
+
+/**
+ * @name 批量数据导入
+ */
+let manyDataInsert = async (ctx) => {
   let computerData = require('../json/computer');
   let phoneData = require('../json/phone');
   let headsetData = require('../json/headset');
@@ -39,35 +46,7 @@ let homeRouter = async (ctx, next) => {
       insertDataArr.push(dataItem)
     });
   })
-  // computerData.itemsArray.forEach((item) => {
-  //   let dataItem = {
-  //     goodsType: 0,
-  //     goodsId: '',
-  //     title: '',
-  //     location: '',
-  //     originalPrice: '0.00',
-  //     discountPrice: '0',
-  //     quantity: 10,
-  //     mainPicPath: '',
-  //     goodRate: '',
-  //     auctionURL: ''
-  //   }
 
-  //   dataItem.goodsType = 1;
-  //   dataItem.goodsId = item.item_id;
-  //   dataItem.title = item.title || item.nick;
-  //   dataItem.location = item.location || item.sellerLoc || item.area;
-  //   dataItem.originalPrice = item.price || item.priceWap;
-  //   dataItem.discountPrice = item.priceWap || item.priceWithRate || item.priceShow.price;
-  //   dataItem.quantity = item.quantity;
-  //   dataItem.mainPicPath = item.pic_path;
-  //   dataItem.goodRate = item.goodRate;
-  //   dataItem.auctionURL = item.auctionURL || item.shopInfo.url
-
-  //   insertDataArr.push(dataItem)
-  // });
-
-  // GoodsModule.deleteMany(null, err => { console.log(err) });
   GoodsModule.insertMany(insertDataArr, (err, doc) => {
     if (err) {
       console.log(err);
@@ -78,7 +57,20 @@ let homeRouter = async (ctx, next) => {
 
   ctx.body = 'home page';
 }
+/**
+ * @name 批量数据删除
+ */
+let manyDataDelete = async (ctx) => {
+  let {name = '', val = ''} = ctx.request.body;
+  let sqlWhere = null
+  if (name && val) {
+    sqlWhere = {[name]: val}
+  }
+  GoodsModule.deleteMany(sqlWhere, err => { console.log(err) });
+}
 
 module.exports = {
-  text: ['GET', '/api/' , homeRouter]
+  text: ['GET', '/api/' , homeRouter],
+  manyDataInsert: ['GET', '/api/db/manyInsert' , manyDataInsert],
+  manyDataDelete: ['GET', '/api/db/manyDelete' , manyDataDelete]
 }
