871257f66a13bf572d0187088984d71b04fb1098
RM_LOG <yezi_log@163.com>
2018-10-28 22:05:11 +0800

    add

--- client/shop-client/src/views/regist/Regist.vue
+++ client/shop-client/src/views/regist/Regist.vue
@@ -1,5 +1,10 @@
 <template>
-  <div></div>
+  <div>
+    <div class="logo"><img src="@/assets/img/logo.png" alt="logo" /></div>
+    <div class="regist-cnt">
+      
+    </div>
+  </div>
 </template>
 
 <script>
