c85875ab23d32e752c45f6b13269804659327b9d
caicai <1841978799@qq.com>
2018-11-28 17:45:05 +0800

    add

--- client/shop-client/src/views/userCenter/UserCenter.vue
+++ client/shop-client/src/views/userCenter/UserCenter.vue
@@ -1,5 +1,27 @@
 <template>
-  <div>
+  <div class="user-main">
+    <div class="user-left">
+      <h4>Electro.</h4>
+      <div class="user-img">
+        <img src="" alt=""/>
+        <p>飞来一只菜鸡</p>
+      </div>
+      <ul class="user-left-list">
+        <li class="user-left-item">
+          <i class="login-icon iconfont icon-user"></i>个人资料
+        </li>
+        <li class="user-left-item">
+          <i class="login-icon iconfont icon-user"></i>全部订单
+        </li>
+        <li class="user-left-item">
+          <i class="login-icon iconfont icon-user"></i>我的收藏
+        </li>
+        <li class="user-left-item">
+          <i class="login-icon iconfont icon-user"></i>收货地址
+        </li>
+      </ul>
+    </div>
+    <div class="user-right"></div>
   </div>
 </template>
 
