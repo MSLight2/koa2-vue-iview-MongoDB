ad95e8ad5ac58e1e230c4e136df7ad5fc14a08a4
caicai <1841978799@qq.com>
2018-12-10 17:41:26 +0800

    add

--- client/shop-client/src/views/userCenter/UserCenter.vue
+++ client/shop-client/src/views/userCenter/UserCenter.vue
@@ -1,27 +1,31 @@
 <template>
-  <div class="user-main">
-    <div class="user-left">
-      <h4>Electro.</h4>
-      <div class="user-img">
-        <img src="" alt=""/>
-        <p>飞来一只菜鸡</p>
+  <div class="user-box">
+    <div class="user-box-bg"></div>
+    <div class="user-box-txt">ELEC<br/>TRO</div>
+    <div class="user-main">
+      <div class="user-left">
+        <h4>Electro.</h4>
+        <div class="user-img">
+          <img src="" alt=""/>
+          <p>飞来一只菜鸡</p>
+        </div>
+        <ul class="user-left-list">
+          <li class="user-left-item">
+            <i class="login-icon iconfont icon-user"></i>个人资料
+          </li>
+          <li class="user-left-item">
+            <i class="login-icon iconfont icon-user"></i>全部订单
+          </li>
+          <li class="user-left-item">
+            <i class="login-icon iconfont icon-user"></i>我的收藏
+          </li>
+          <li class="user-left-item">
+            <i class="login-icon iconfont icon-user"></i>收货地址
+          </li>
+        </ul>
       </div>
-      <ul class="user-left-list">
-        <li class="user-left-item">
-          <i class="login-icon iconfont icon-user"></i>个人资料
-        </li>
-        <li class="user-left-item">
-          <i class="login-icon iconfont icon-user"></i>全部订单
-        </li>
-        <li class="user-left-item">
-          <i class="login-icon iconfont icon-user"></i>我的收藏
-        </li>
-        <li class="user-left-item">
-          <i class="login-icon iconfont icon-user"></i>收货地址
-        </li>
-      </ul>
+      <div class="user-right"></div>
     </div>
-    <div class="user-right"></div>
   </div>
 </template>
 
